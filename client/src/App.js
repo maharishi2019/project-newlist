e891be3025f0a7e508cc73f3bd1c2a02c3c3501a
maharishi2019 <55564095+maharishi2019@users.noreply.github.com>
2021-01-22 08:56:54 -0800

    firebase added to project

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+//import of firebase (database)
+import { FirebaseDatabaseProvider } from "@react-firebase/database";
+
 //import of routing libraries
 import {
     BrowserRouter as Router,
@@ -33,35 +36,37 @@ import Signup from "./Signup";
 
 export default function App() {
     return (
-        <Router>
-            <ul>
-                <li>
-                    <LinkContainer to="/">
-                        <Nav.Link>Home</Nav.Link>
-                    </LinkContainer>
-                </li>
-                <li>
-                    <LinkContainer to="/login">
-                        <Nav.Link>Login</Nav.Link>
-                    </LinkContainer>
-                </li>
-                <li>
-                    <LinkContainer to="/signup">
-                        <Nav.Link>Signup</Nav.Link>
-                    </LinkContainer>
-                </li>
-            </ul>
-            <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route exact path="/login">
-                    <Login />
-                </Route>
-                <Route exact path="/signup">
-                    <Signup />
-                </Route>
-            </Switch>
-        </Router >
+        <FirebaseDatabaseProvider>
+            <Router>
+                <ul>
+                    <li>
+                        <LinkContainer to="/">
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                    </li>
+                    <li>
+                        <LinkContainer to="/login">
+                            <Nav.Link>Login</Nav.Link>
+                        </LinkContainer>
+                    </li>
+                    <li>
+                        <LinkContainer to="/signup">
+                            <Nav.Link>Signup</Nav.Link>
+                        </LinkContainer>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route exact path="/login">
+                        <Login />
+                    </Route>
+                    <Route exact path="/signup">
+                        <Signup />
+                    </Route>
+                </Switch>
+            </Router >
+        </FirebaseDatabaseProvider>
     );
 }
