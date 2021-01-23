ff49f3c5330d65dfdd7bd7a57462de9ad63cf8da
maharishi2019 <55564095+maharishi2019@users.noreply.github.com>
2021-01-22 20:32:39 -0800

    login and signup functionalities added
    needs a lot more work though

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
 import firebase from "./Firebase";
 import "firebase/auth";
+import {
+    BrowserRouter as Router,
+    NavLink,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
+
+import Home from "./Home";
 
 class GoogleLogin extends Component {
+    state = { loggedIn: false }
     onSubmit = () => {
         var provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth()
@@ -28,18 +40,83 @@ class GoogleLogin extends Component {
                 var credential = error.credential;
                 // ...
             });
+        <Switch>
+            <Redirect exact from="/login" to="/" />
+            <Route path="/login">
+                <Login />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
     }
     render() {
         return (
-            <button type="button" onClick={this.onSubmit}>
-                Google Login
-            </button>
+            <div>
+                <button type="button" onClick={this.onSubmit}>
+                    Google Login
+                </button>
+            </div>
+        );
+    };
+}
+
+class TraditionalLogin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: '', password: '' };
+
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleEmailChange(event) {
+        this.setState({ email: event.target.value });
+    }
+
+    handlePasswordChange(event) {
+        this.setState({ password: event.target.value });
+    }
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.email);
+        event.preventDefault();
+
+        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then((user) => {
+                // Signed in 
+                // ...
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group id="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" value={this.state.email} onChange={this.handleEmailChange} required></Form.Control>
+                    </Form.Group>
+                    <Form.Group id="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" value={this.state.password} onChange={this.handlePasswordChange} required></Form.Control>
+                    </Form.Group>
+                    <Button type="submit">Login</Button>
+                </Form>
+            </div>
         );
     };
 }
 
 export default function Login() {
     return (
-        <GoogleLogin />
+        <div>
+            <GoogleLogin />
+            <TraditionalLogin />
+        </div>
     );
 }
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+import firebase from "./Firebase";
+import "firebase/auth";
+import {
+    BrowserRouter as Router,
+    NavLink,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
 
+import Home from "./Home";
+
+class TraditionalSignup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: '', password: '' };
+
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleEmailChange(event) {
+        this.setState({ email: event.target.value });
+    }
+
+    handlePasswordChange(event) {
+        this.setState({ password: event.target.value });
+    }
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.email);
+        event.preventDefault();
+
+        firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then((user) => {
+                // Signed in 
+                // ...
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // ..
+            });
+        firebase.auth().currentUser.sendEmailVerification()
+            .then(() => {
+                // Email verification sent!
+                // ...
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group id="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" value={this.state.email} onChange={this.handleEmailChange} required></Form.Control>
+                    </Form.Group>
+                    <Form.Group id="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" value={this.state.password} onChange={this.handlePasswordChange} required></Form.Control>
+                    </Form.Group>
+                    <Button type="submit">Signup</Button>
+                </Form>
+            </div>
+        );
+    };
+}
 export default function Signup() {
     return (
-        <h1>Signup</h1>
+        <TraditionalSignup />
     );
 }
